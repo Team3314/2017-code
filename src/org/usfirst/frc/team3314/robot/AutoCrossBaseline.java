0c8126fa9c7d8b138c6a8b6a2d177810ce3e6b0f
crezeshox <crezeshox@gmail.com>
2017-01-27 20:45:56 -0500

    imp't changes

--- src/org/usfirst/frc/team3314/robot/AutoCrossBaseline.java
+++ src/org/usfirst/frc/team3314/robot/AutoCrossBaseline.java
@@ -1,8 +1,5 @@
 package org.usfirst.frc.team3314.robot;
 
-//import com.ctre.CANTalon.*;
-
-//import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 enum autoCrossBaselineStates {
@@ -34,7 +31,6 @@ public class AutoCrossBaseline {
 	public void update() {
 		//sees whether requirements to go to next state are fulfilled and switches states if necessary,
 		//executes code assigned to each state, counts down time every 20ms
-		//distance = robot.tdt.getDistance();	
 		calcNext();
 		doTransition();
 		currentState = nextState;
@@ -46,15 +42,13 @@ public class AutoCrossBaseline {
 	public void calcNext() {
 		nextState = currentState;
 		
-		//most either go to next state immediately or after timer finishes, 1 goes after encoder pos reaches about 25k
 		switch (currentState) {
 		case START:
 			robot.hal.gyro.reset();
 			nextState = autoCrossBaselineStates.DRIVE;
 			break;
 		case DRIVE:
-			if (time <= 0 || /*robot.tdt.lDriveTalon1.getEncPosition() > 25000 &&*/ robot.tdt.rDriveTalon1.getEncPosition() > 25000){
-			//desiredDistance - distance <= tolerance) {
+			if (/*robot.tdt.lDriveTalon1.getEncPosition() > &&*/ robot.tdt.rDriveTalon1.getEncPosition() > (93.25*robot.encoderConversionFactor)){
 				nextState = autoCrossBaselineStates.STOP;
 			}
 			break;
@@ -70,16 +64,15 @@ public class AutoCrossBaseline {
 	
 	public void doTransition() {
 		if (currentState == autoCrossBaselineStates.START && nextState == autoCrossBaselineStates.DRIVE) {
-			//robot drives straight forward at max speed, 10 sec
+			//robot drives straight forward at max speed, 4 sec
 			robot.tdt.setDriveAngle(robot.hal.gyro.angle());
 			robot.tdt.setDriveTrainSpeed(1);
-			time = 500;
+			time = 200;
 		}
 		
 		if (currentState == autoCrossBaselineStates.DRIVE && nextState == autoCrossBaselineStates.STOP) {
 			//stops robot, 1 sec
 			robot.tdt.setDriveTrainSpeed(0);
-			//robot.hal.intake.set(Value.kReverse);
 			time = 50;
 		}
 	}
