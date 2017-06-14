da2efb7ce83369e23b47143468fe83f52eaf2387
Filip3314 <fmusial@outlook.com>
2017-06-14 16:24:53 -0400

    Commented and cleaned up

--- src/org/usfirst/frc/team3314/robot/Turret.java
+++ src/org/usfirst/frc/team3314/robot/Turret.java
@@ -9,12 +9,14 @@ public class Turret {
 	
 	public Turret(Robot myRobot) {
 		robot = myRobot;
-
+		
 		robot.hal.turretTalon.changeControlMode(TalonControlMode.Position);
 		robot.hal.turretTalon.setFeedbackDevice(FeedbackDevice.CtreMagEncoder_Relative);
 		robot.hal.turretTalon.setPID(Constants.kTurret_kP, Constants.kTurret_kI, Constants.kTurret_kD,
 		Constants.kTurret_kF, Constants.kTurret_IZone, Constants.kTurret_RampRate, Constants.kTurret_Profile);
+		//Caps turret motor voltage to prevent overshooting
 		robot.hal.turretTalon.configMaxOutputVoltage(8);
+		//Prevents turret from trying to turn past hard stops
 		robot.hal.turretTalon.enableForwardSoftLimit(true);
 		robot.hal.turretTalon.setForwardSoftLimit(7.55);
 		robot.hal.turretTalon.enableReverseSoftLimit(true);
@@ -22,7 +24,7 @@ public class Turret {
 	}
 	
 	public void update() {
-		//talon turns motor to target
+		//Removes I term from turret PID controller when error is under 1000 ticks to prevent buildup of I term and overshoot on long turns
 		if(Math.abs(robot.hal.turretTalon.getClosedLoopError()) > 1000 ) {
 			robot.hal.turretTalon.setPID(Constants.kTurret_kP,  0,  Constants.kTurret_kD);
 		}
@@ -32,7 +34,8 @@ public class Turret {
 		turretPosition = robot.hal.turretTalon.getPosition();
 		robot.hal.turretTalon.set(desiredTarget);
 	}
-	
+	/* Resets turret position based on red/blue switch, as the turret is supposed to be turned to the left(zero) 
+	when starting on the blue side of the field and to the right (7.55) when starting on the red side of the field*/
 	public void reset() {
 		if (robot.blueRequest) {
 			robot.hal.turretTalon.setPosition(0);
@@ -44,7 +47,7 @@ public class Turret {
 	}
 	
 	public double getEncError(double degError) {
-		//converts error in degrees to encoder ticks, then adds it to current position to calculate desired target
+		//Takes an input in degrees and converts it to encoder ticks so that it can be fed into the encoder
 		double encError = degError * Constants.kDegToEncTicksConvFactor;
 		desiredTarget = encError + robot.hal.turretTalon.getPosition();
 		return desiredTarget;
