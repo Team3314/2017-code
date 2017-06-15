6f4e80a071b63ad48e5fbbe9a41007cf84dac0c0
Filip3314 <fmusial@outlook.com>
2017-06-14 20:12:53 -0400

    Commented

--- src/org/usfirst/frc/team3314/robot/GyroPIDOutput.java
+++ src/org/usfirst/frc/team3314/robot/GyroPIDOutput.java
@@ -2,6 +2,9 @@ package org.usfirst.frc.team3314.robot;
 
 import edu.wpi.first.wpilibj.PIDOutput;
 
+/*Takes the output of the gyro PID controller and creates a variable from it which is used to speed up one side and slow down the other
+to keep the robot driving straight in gyrolock. */
+
 public class GyroPIDOutput implements PIDOutput {
 	Robot robot;
 	double turnSpeed = 0;
